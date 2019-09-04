fc05e80e257e3c10ce0b8ddaf1b8de18a11780e4
jpward <ward.j2@gmail.com>
2019-09-04 10:39:45 -0400

    Updates to remove auto_gen.dart

--- lib/auto_gen.dart
+++
@@ -1,7 +0,0 @@
-import 'package:encrypt/encrypt.dart' as encrypt;
-String userPoolId = 'us-east-1_NeYs2Npv8';
-String clientPoolId = '1f7foa96janp89b2ncdjjoaejn';
-String guestName = 'guest';
-String guestPass = 'BGT%4rfv';
-final key = encrypt.Key.fromUtf8('afahefa674qio62i2nfazf2hgqlc9g74');
-String endpoint311base = 'https://api.socialtorch.org';
