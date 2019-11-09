ce9fdf5c667595d08eb87cc8c6050b7605eebb4d
jpward <ward.j2@gmail.com>
2019-11-08 20:20:24 -0500

    Updated image gen script to take in extra options

--- prebuild.sh
+++ prebuild.sh
@@ -63,27 +63,27 @@ EOF
 #Generate Icons/Images
 TEMPLATE_IMG="$HERE/images/logo_512x512.png"
 GEN_IMAGES=" \
-$HERE/images/logo_512x512.jpg,512 \
-$HERE/android/app/src/main/res/mipmap-hdpi/launcher_icon.png,72 \
-$HERE/android/app/src/main/res/mipmap-mdpi/launcher_icon.png,48 \
-$HERE/android/app/src/main/res/mipmap-xhdpi/launcher_icon.png,96 \
-$HERE/android/app/src/main/res/mipmap-xxhdpi/launcher_icon.png,144 \
-$HERE/android/app/src/main/res/mipmap-xxxhdpi/launcher_icon.png,192 \
-$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-1024x1024@1x.png,1024 \
-$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@1x.png,20 \
-$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@2x.png,40 \
-$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@3x.png,60 \
-$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@1x.png,29 \
-$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@2x.png,58 \
-$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@3x.png,87 \
-$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@1x.png,40 \
-$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@2x.png,80 \
-$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@3x.png,120 \
-$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-60x60@2x.png,120 \
-$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-60x60@3x.png,180 \
-$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-76x76@1x.png,76 \
-$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-76x76@2x.png,152 \
-$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-83.5x83.5@2x.png,167 \
+$HERE/images/logo_512x512.jpg,512,-blur,0x0.75 \
+$HERE/android/app/src/main/res/mipmap-hdpi/launcher_icon.png,72, \
+$HERE/android/app/src/main/res/mipmap-mdpi/launcher_icon.png,48, \
+$HERE/android/app/src/main/res/mipmap-xhdpi/launcher_icon.png,96, \
+$HERE/android/app/src/main/res/mipmap-xxhdpi/launcher_icon.png,144, \
+$HERE/android/app/src/main/res/mipmap-xxxhdpi/launcher_icon.png,192, \
+$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-1024x1024@1x.png,1024, \
+$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@1x.png,20, \
+$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@2x.png,40, \
+$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@3x.png,60, \
+$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@1x.png,29, \
+$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@2x.png,58, \
+$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@3x.png,87, \
+$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@1x.png,40, \
+$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@2x.png,80, \
+$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@3x.png,120, \
+$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-60x60@2x.png,120, \
+$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-60x60@3x.png,180, \
+$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-76x76@1x.png,76, \
+$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-76x76@2x.png,152, \
+$HERE/ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-83.5x83.5@2x.png,167, \
 "
 # md5 or md5sum? Git Bash on Windows uses md5sum
 MD5CMD="md5 -q"
@@ -113,7 +113,9 @@ if $GEN_EM; then
       for f in $GEN_IMAGES; do
         IMG=$(echo $f | cut -d',' -f1)
         SZ=$(echo $f | cut -d',' -f2)
-        gm convert -resize ${SZ}x${SZ} $TEMPLATE_IMG $IMG
+        EXTRA_OPS=$(echo $f | cut -d',' -f3- | sed 's/,/ /g')
+     
+        gm convert -resize ${SZ}x${SZ} ${EXTRA_OPS} $TEMPLATE_IMG $IMG
       done
     )
   else
